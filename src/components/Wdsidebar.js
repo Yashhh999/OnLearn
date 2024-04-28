295b81c1bba76468d7b5288e4c0526b33dde585c
Yash Gupta <gupta25yash@gmail.com>
2024-04-28 15:58:28 +0530

    Improved sidebar 2.0

--- src/components/Wdsidebar.js
+++ src/components/Wdsidebar.js
@@ -3,46 +3,54 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Wdsidebar() {
-    const pathName=usePathname();
+    const pathName = usePathname();
     return (
         <>
             <div className="h-screen w-1/5 bg-white border-r-2 p-1 m-0">
 
-                
-            <div className={pathName === "/learn/webdev/html" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
-                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium ">
-                        <Link href="/learn/webdev/html">HTML</Link>
+                <Link href="/learn/webdev/html">
+                    <div className={pathName === "/learn/webdev/html" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
+                        <div className="mt-10 mx-10 text-xl text-gray-600 font-medium ">
+                            HTML
+                        </div>
                     </div>
-                </div>
+                </Link>
 
-                <div className={pathName === "/learn/webdev/css" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
-                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium ">
-                        <Link href="/learn/webdev/css" className="">CSS</Link>
+                <Link href="/learn/webdev/css" className="">
+                    <div className={pathName === "/learn/webdev/css" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
+                        <div className="mt-10 mx-10 text-xl text-gray-600 font-medium ">
+                            CSS
+                        </div>
+
                     </div>
-                    
-                </div>
+                </Link>
 
-                <div className={pathName === "/learn/webdev/js" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
-                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium">
-                        <Link href="/learn/webdev/js" className="mx-1" >JavaScript</Link>
+                <Link href="/learn/webdev/js" className="" >
+                    <div className={pathName === "/learn/webdev/js" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
+                        <div className="mt-10 mx-10 text-xl text-gray-600 font-medium">
+                            JavaScript
+                        </div>
+
                     </div>
-                    
-                </div>
+                </Link>
 
-                <div className={pathName === "/learn/webdev/extra" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
-                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium ">
-                        <Link href="/learn/webdev/extra" className="mx-1" >Extra</Link>
+                <Link href="/learn/webdev/extra" className="" >
+                    <div className={pathName === "/learn/webdev/extra" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
+                        <div className=" mx-10 text-xl text-gray-600 font-medium ">
+                            Extra
+                        </div>
+
                     </div>
-                    
-                </div>
+                </Link>
 
+                <Link href="/learn/webdev/projects" className="">
+                    <div className={pathName === "/learn/webdev/projects" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
+                        <div className=" mx-10 text-xl text-gray-600 font-medium w-28">
+                            Projects
+                        </div>
 
-                <div className={pathName === "/learn/webdev/projects" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500 border-b-2 border-gray-500 hover:bg-violet-500" : "bg-blue-200 hover:bg-violet-300 transform: scale-100 duration-500 rounded m-2 h-8 border-b-2  border-gray-500"}>
-                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium w-28">
-                        <Link href="/learn/webdev/projects" className="py-1">Projects</Link>
                     </div>
-                    
-                </div>
+                </Link>
 
 
             </div>
