daf3e2993d45ed0f4acc853740d2b5f0210577dd
Yash Gupta <gupta25yash@gmail.com>
2024-04-28 12:48:21 +0530

    Made wdsidebar better

--- src/components/Wdsidebar.js
+++ src/components/Wdsidebar.js
@@ -1,57 +1,51 @@
+'use client'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Wdsidebar() {
+    const pathName=usePathname();
     return (
-        <div className="h-screen w-1/5 bg-sky-100 p-1 m-0">
+        <>
+            <div className="h-screen w-1/5 bg-sky-100 p-1 m-0">
+
+                
+            <div className={pathName === "/learn/webdev/html" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500" : "bg-blue-200 transform: scale-100 duration-500 rounded m-2 h-8"}>
+                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium border-b-2 w-14 border-gray-500">
+                        <Link href="/learn/webdev/html">HTML</Link>
+                    </div>
+                </div>
+
+                <div className={pathName === "/learn/webdev/css" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500" : "bg-blue-200 transform: scale-100 duration-500 rounded m-2 h-8"}>
+                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium border-b-2 w-14 border-gray-500">
+                        <Link href="/learn/webdev/css" className="">CSS</Link>
+                    </div>
+                    
+                </div>
+
+                <div className={pathName === "/learn/webdev/js" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500" : "bg-blue-200 transform: scale-100 duration-500 rounded m-2 h-8"}>
+                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium border-b-2 w-28 border-gray-500">
+                        <Link href="/learn/webdev/js" className="mx-1" >JavaScript</Link>
+                    </div>
+                    
+                </div>
+
+                <div className={pathName === "/learn/webdev/extra" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500" : "bg-blue-200 transform: scale-100 duration-500 rounded m-2 h-8"}>
+                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium border-b-2 w-28 border-gray-500">
+                        <Link href="/learn/webdev/extra" className="mx-1" >Extra</Link>
+                    </div>
+                    
+                </div>
 
 
-            <div className="bg-blue-200 rounded m-2">
-            <div className="mt-10 mx-10 text-xl text-gray-500 font-medium border-b-2 w-14 border-gray-500">
-                <Link href="/">HTML</Link>
-            </div>
-            <div className="my-2 mx-14 bg-blue-300 w-40 rounded">
-                <Link href="/" className="m-1">HTML One Shot</Link>
-            </div>
-            </div>
-
-            <div className="bg-blue-200 rounded m-2">
-            <div className="mt-10 mx-10 text-xl text-gray-500 font-medium border-b-2 w-14 border-gray-500">
-                <Link href="/" className="">CSS</Link>
-            </div>
-            <div className="my-2 mx-14 bg-blue-300 w-40 rounded">
-                <Link href="/" className="m-1">CSS One Shot</Link>
-            </div>
-            </div>
-
-            <div className="bg-blue-200 rounded m-2">
-            <div className="mt-10 mx-10 text-xl text-gray-500 font-medium border-b-2 w-28 border-gray-500">
-                <Link href="/" className="mx-1" >JavaScript</Link>
-            </div>
-            <div className="my-2 mx-14 bg-blue-300 w-40 rounded">
-                <Link href="/" className="m-1">JS Part 1</Link>
-            </div>
-            <div className="my-2 mx-14 bg-blue-300 w-40 rounded">
-                <Link href="/" className="m-1">JS Part 2</Link>
-            </div>
-            <div className="my-2 mx-14 bg-blue-300 w-40 rounded">
-                <Link href="/" className="m-1">JS Part 3</Link>
-            </div>
-            </div>
+                <div className={pathName === "/learn/webdev/projects" ? "bg-violet-400 rounded m-2 h-8 transform: scale-105 duration-500" : "bg-blue-200 transform: scale-100 duration-500 rounded m-2 h-8"}>
+                    <div className="mt-10 mx-10 text-xl text-gray-500 font-medium border-b-2 w-20 border-gray-500">
+                        <Link href="/learn/webdev/projects" className="py-1">Projects</Link>
+                    </div>
+                    
+                </div>
 
 
-            <div className="bg-blue-200 rounded m-2">
-            <div className="mt-10 mx-10 text-xl text-gray-500 font-medium border-b-2 w-20 border-gray-500">
-                <Link href="/" className="">Projects</Link>
             </div>
-            <div className="my-2 mx-14 bg-blue-300 w-40 rounded">
-                <Link href="/" className="m-1">Project 1</Link>
-            </div>
-            <div className="my-2 mx-14 bg-blue-300 w-40 rounded">
-                <Link href="/" className="m-1">Project 2</Link>
-            </div>
-            </div>
-
-
-        </div>
+        </>
     );
 }
